2f98c9adf11bec24ffbc4e0dfabb08f3ca6df188
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-04-28 17:08:25 +0100

    Sends the file to Sequencescape using a simple post

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'http://rubygems.org'
+
+gem 'sequencescape-client-api', '>= 0.2.5',
+  # Should be switched back to sanger + production for deployment
+  :github  => 'JamesGlover/sequencescape-client-api',
+  :branch  => 'dual_indexing_r32',
+  :require => 'sequencescape'
--- process_files.rb
+++ process_files.rb
@@ -5,6 +5,9 @@ require 'fileutils'
 
 require 'net/http'
 
+SS_URL = "http://localhost:3000/api/1/"
+SS_KEY = "development"
+
 
 ARCHIVE_FOLDER = 'archive/'
 INBOX_FOLDER = 'example_files/'
@@ -18,7 +21,6 @@ class MoveFile
     @path = INBOX_FOLDER
   end
 
-  private
 
   def filename
     @filename
@@ -49,8 +51,8 @@ class MoveFile
     File.rename(@path + lock_filename, @path + filename) if locked?
   end
 
-  def archive
-    FileUtils.mv(@path + lock_filename, ARCHIVE_FOLDER + filename)
+  def archive(tag="")
+    FileUtils.mv(@path + lock_filename, ARCHIVE_FOLDER + filename + tag)
     @path = ARCHIVE_FOLDER
   end
 
@@ -68,21 +70,60 @@ def get_files_from_folder(folder)
   Dir.entries(folder)
 end
 
-def headers
-  {
-    "Content-Type" => "text/uuid"
-  }
+class ApiConnection
+  attr_reader :host, :port
+
+  def initialize(host, port)
+    @host = host
+    @port = port
+  end
+
+  def headers
+
+  end
+
+  def request_from_path(path, headers)
+    Net::HTTP.new(@host, @port).get2(path, headers)
+  end
+
+  def ss_api
+    self.api ||= Sequencescape::Api.new(:url => SS_URL, :authorisation => SS_KEY)
+  end
+
 end
 
 def request_uuid(barcode)
-  httpcall = Net::HTTP.new("localhost", "4567")
-  response = httpcall.get2("/assays/#{barcode}/input", headers)
+  response = ApiConnection.new("localhost", "4567").request_from_path("/assays/#{barcode}/input", { "Content-Type" => "text/uuid" })
   uuid = response.body unless response.is_a?(Net::HTTPNotFound)
 end
 
-def upload_qc_file_to_ss(uuid, filename)
+
+def upload_qc_file_to_ss_with_api(uuid, filename)
+  ss_api.find(uuid).qc_files.create_from_file(filename, filename)
 end
 
+def perform_for_file(url, file, filename, headers)
+  uri = URI.parse(url)
+  file_content = file.read
+  puts "file reading: #{file_content}"
+  Net::HTTP.start(uri.host, uri.port) do |connection|
+    #connection.read_timeout = "3000"
+    file_headers = headers.merge!({'Content-Disposition'=> "form-data; filename=\"#{filename}\""})
+    request = Net::HTTP.const_get("Post").new(uri.request_uri, file_headers)
+    request.content_type = 'sequencescape/qc_file'
+    #request.body         = body.to_json
+    request.body         = file_content
+    connection.request(request)
+    #yield(connection.request(request))
+  end
+end
+
+def upload_qc_file_to_ss_with_http(uuid, current_movefile)
+  perform_for_file(SS_URL+uuid+"/qc_files", File.open(current_movefile.absolute_filename, "r"), current_movefile.filename,  {
+    "X-Sequencescape-Client-ID" => SS_KEY,
+    "Sequencescape-Client-ID" => SS_KEY
+    })
+end
 
 def process_file(filename)
   current_movefile = MoveFile.new(filename)
@@ -90,12 +131,12 @@ def process_file(filename)
 
   uuid = request_uuid(current_movefile.get_barcode)
 
-  puts uuid
-
-  upload_qc_file_to_ss(uuid, filename)
-
-
-  #current_movefile.archive
+  if uuid
+    upload_qc_file_to_ss_with_http(uuid, current_movefile)
+    #current_movefile.archive
+  else
+    #current_movefile.archive(".unknown_uuid")
+  end
 ensure
   current_movefile.unlock
 end
